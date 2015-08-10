6f94aa0b1f8dde6e07a867a74dc73f634d7ada99
jordan navratil & jenna schold navratil <student@epicodus.com>
2015-08-10 15:33:33 -0700

    add twig files and update composer

--- app/app.php
+++ app/app.php
@@ -12,12 +12,11 @@
         return $app['twig']->render('form.twig');
     });
 
-    $app->get("/view_title_case", function () use($app) {
+    $app->get("/view_title_case", function() use($app) {
         $my_TitleCaseGenerator = new TitleCaseGenerator;
         $title_cased_phrase = $my_TitleCaseGenerator->makeTitleCase($_GET['phrase']);
-        return $app['twig']->render('title_cased.twig', array('result => $title_cased_phrase'));
+        return $app['twig']->render('title_cased.twig', array('result' => $title_cased_phrase));
     });
 
     return $app;
-
- ?>
+?>
